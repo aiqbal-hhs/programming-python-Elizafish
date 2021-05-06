d9ff4b48ad2530eeadf5ca7c3f52384c3c061ae1
Elizafish <80366280+Elizafish@users.noreply.github.com>
2021-05-06 14:54:55 +1200

    Add files via upload

---
+++ Test doc.py
@@ -0,0 +1,7 @@
+print("Joe " + "Blogs")
+print("Elizabeth " + "Fletcher " + "16/"+"12/"+"2006")
+print("0110100001100101011011000110110001101111")
+
+name = input("What is your full name?")
+age = input("Where do you live?")
+print("So your name is {} and you live at {}." .format (name, age))
