af4b37e15a07e79f8f0689ede1785c9beaec0453
Elizafish <80366280+Elizafish@users.noreply.github.com>
2021-05-10 10:56:12 +1200

    Add files via upload

---
+++ format-function-may10.py
@@ -0,0 +1,11 @@
+name = input("What is your name?")
+age = input("How old are you?")
+occupation = input("Do you have a job?")
+home = input("Which city do you live in?")
+
+if occupation yes print("{} is a nice age to be, {}! It must be nice to be living and working in {}.".format(age, name, home))
+else:
+print("{} is a nice age to be, {}! It must be nice to be living in {} and not having to work.".format(age, name, home))
+print("{} is a nice age to be, {}! It must be nice to be living in {} and working as a {}.".format(age, name, home, occupation))
+
+
